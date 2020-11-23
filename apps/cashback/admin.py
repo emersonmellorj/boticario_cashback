7c44d732f41e93b8c79d46b000f74d41dd201d8f
Emerson Mello <emersonmello@Emersons-MacBook-Air.local>
2020-11-23 16:20:13 -0300

    Instalação dos pacotes iniciais, criação do projeto e do app e criação do modelo Usuario.

---
+++ apps/cashback/admin.py
@@ -0,0 +1,79 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+
+from .models import Usuarios
+
+
+class UsuarioCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Usuarios
+        fields = ('email', 'firstname', 'lastname', 'cpf')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UsuarioChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Usuarios
+        fields = ('email', 'password', 'firstname', 'lastname', 'cpf',
+                  'is_active', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+@admin.register(Usuarios)
+class UsuarioAdmin(BaseUserAdmin):
+    form = UsuarioChangeForm
+    add_form = UsuarioCreationForm
+
+    list_display = ('firstname', 'lastname', 'email', 'cpf', 'is_admin')
+    list_filter = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('firstname', 'lastname', 'cpf',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'firstname', 'lastname', 'cpf', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
---
+++ apps/cashback/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, firstname, lastname, cpf, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            cpf=cpf,
+            firstname=firstname,
+            lastname=lastname,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, firstname, lastname, cpf, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            cpf=cpf,
+            firstname=firstname,
+            lastname=lastname
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class Usuarios(AbstractBaseUser):
+    firstname = models.CharField(max_length=50, null=False, blank=False)
+    lastname = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(
+        verbose_name='E-mail',
+        max_length=255,
+        unique=True,
+    )
+    cpf = models.CharField("CPF", max_length=14)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstname', 'lastname', 'cpf']
+
+    objects = UsuarioManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    class Meta:
+        db_table = "usuarios"
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
